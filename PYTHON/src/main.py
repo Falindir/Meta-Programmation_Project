f7982a62645a2f244805d99e1e32c4b7bf83d674
Gouga34 <imala34@hotmail.fr>
2016-01-25 09:57:25 +0100

    Python : correction memos

--- PYTHON/src/main.py
+++ PYTHON/src/main.py
@@ -1,8 +1,9 @@
 class MemoClass(type):
-    instance = []
 
     def __new__(mcs, name, bases, dct):
-        return super(MemoClass, mcs).__new__(mcs, name, bases, dct)
+        s = super(MemoClass, mcs).__new__(mcs, name, bases, dct)
+        s.instances = []
+        return s
 
     def __init__(cls, name, bases, dct):
         super(MemoClass, cls).__init__(name, bases, dct)
@@ -10,31 +11,30 @@ class MemoClass(type):
 
     def __call__(cls, *args, **kwds):
         s = type.__call__(cls, *args, **kwds)
-        cls.instance.append(s)
+        cls.instances.append(s)
         return s
 
     def print_instance(self):
 
-        print("\nInstances of : ", self.__name__, " - size : ", self.instance.__len__())
+        print("\nInstances de ", self.__name__, " (taille ", self.instances.__len__(),") : ")
 
-        for inst in self.instance:
+        for inst in self.instances:
             inst.who()
 
         pass
 
     def free_instance(self, cls):
 
-        print("\nFree instance : ")
+        print("\nFree : ")
         cls.who()
 
-        if cls in self.instance:
-            self.instance.remove(cls)
+        if cls in self.instances:
+            self.instances.remove(cls)
 
         pass
 
 
 class MemoObject(object, metaclass=MemoClass):
-
 
     def __init__(self, name):
         self.name = name
@@ -47,55 +47,14 @@ class MemoObject(object, metaclass=MemoClass):
 
 
 class Person(MemoObject):
-	instance = []
 
     def __init__(self, name):
         super().__init__(name)
 
 
 class Animal(MemoObject):
-	instance = []
 
     def __init__(self, name):
         super().__init__(name)
 
-
-print("Main")
-
-MemoObject.print_instance()
-
-m1 = MemoObject("M1")
-print("\nM1 : ")
-m1.who()
-
-MemoObject.print_instance()
-
-m2 = MemoObject("M2")
-print("\nM2 : ")
-m2.who()
-
-MemoObject.print_instance()
-
-m1.free()
-
-print("PERSON")
-
-MemoObject.print_instance()
-Person.print_instance()
-
-p1 = Person("P1")
-print("\nP1 : ")
-p1.who()
-
-MemoObject.print_instance()
-Person.print_instance()
-Animal.print_instance()
-
-a1 = Animal("A1")
-print("\nA1 : ")
-a1.who()
-
-MemoObject.print_instance()
-Person.print_instance()
-Animal.print_instance()
 
