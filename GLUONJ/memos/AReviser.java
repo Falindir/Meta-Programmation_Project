6128a191373c74c892641e6f593ad92cef6b8a9f
Gouga34 <imala34@hotmail.fr>
2016-01-26 08:22:45 +0100

    gluonj : ajout init dans constructeur

--- GLUONJ/memos/AReviser.java
+++ GLUONJ/memos/AReviser.java
@@ -4,7 +4,7 @@ import test.A;
 import java.util.HashSet;
 import java.util.Set;
 
-public reviser AReviser extends test.A {
+public reviser AReviser extends A {
 
 	private static Set<A> instances = new HashSet<A>();
 
@@ -16,6 +16,12 @@ public reviser AReviser extends test.A {
 
 	public void whoamI() {
 		System.out.println("Class A reviser");
+		System.out.println(instances.toString());
+	}
+	
+	public void init() {
+		super.init();
+		register();
 	}
 
 	public void register() {
--- GLUONJ/test/A.java
+++ GLUONJ/test/A.java
@@ -1,9 +1,15 @@
 package test;
 
-public class A {
+import memos.MemoObject;
 
-	public void A() {
-		
+public class A extends MemoObject{
+
+	public A() {
+		super();
+	}
+	
+	public void init() {
+		System.out.println("Init Class A");
 	}
 
 	public void whoamI() {
@@ -11,6 +17,12 @@ public class A {
 	}
 
 	public static void main(String[] args) {
-		new A().whoamI();
+		A a = new A();
+		a.whoamI();
+		A a2 = new A();
+		a2.whoamI();
+		
+		a.free();
+		a2.whoamI();
 	}
 }
