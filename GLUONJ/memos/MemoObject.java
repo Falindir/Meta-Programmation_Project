7096248ba9867df0df8a617140b8db9632a47c58
Gouga34 <imala34@hotmail.fr>
2016-01-26 08:24:50 +0100

    GluonJ

---
+++ GLUONJ/memos/MemoObject.java
@@ -0,0 +1,16 @@
+package memos;
+
+public class MemoObject {
+
+	public MemoObject() {
+		init();
+	}
+	
+	public void init() {
+		System.out.println("Init Class MemoObject");
+	}
+	
+	public void whoamI() {
+		System.out.println("Class MemoObject");
+	}
+}
